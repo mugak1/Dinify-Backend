96058fcb1fd0206d4714c0bc7286a1a8ff4a3528
Esau Lwanga <31913619+lemndev@users.noreply.github.com>
2024-02-08 12:33:02 +0300

    bootstrap misc_app

--- dinify_backend/settings.py
+++ dinify_backend/settings.py
@@ -43,7 +43,8 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
 
     # the project applications
-    'users_app'
+    'users_app',
+    'misc_app'
 ]
 
 MIDDLEWARE = [
---
+++ misc_app/apps.py
@@ -0,0 +1,6 @@
+from django.apps import AppConfig
+
+
+class MiscAppConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'misc_app'
