9c640f96567188194154c3461a335fe88c6fe5a8
Esau Lwanga <31913619+lemndev@users.noreply.github.com>
2024-05-27 16:53:42 +0300

    handle recording a manual transactionr record

--- finance_app/controllers/process_order_payment.py
+++ finance_app/controllers/process_order_payment.py
@@ -1,11 +1,13 @@
-from django.db import transaction
+from decimal import Decimal
 from finance_app.models import DinifyTransaction
 from orders_app.models import Order
 from dinify_backend.configss.string_definitions import (
     TransactionType_OrderPayment,
     TransactionStatus_Success,
     PaymentStatus_Paid,
+    OrderItemStatus_Served
 )
+from misc_app.controllers.clean_amount import clean_amount
 from dinify_backend.configss.messages import OK_ORDER_PAYMENT_PROCESSED
 
 
@@ -19,12 +21,16 @@ def process_order_payment(
     order = Order.objects.select_for_update().get(id=transaction_record.order.id)
     if transaction_status == TransactionStatus_Success:
         # TODO check if the cumulative amount paid is equal to the order amount
-        order.total_paid = order
+        total_paid = order.total_paid
+        total_paid += transaction_record.transaction_amount
+        balance_payable = clean_amount(Decimal(order.total_cost)) - total_paid
+        order.total_paid = total_paid
+        order.balance_payable = balance_payable
 
-        
-        order.payment_status = PaymentStatus_Paid
-        if order.order_status == "Served":
-            order.order_status = "Paid"
+        if order.balance_payable <= clean_amount(Decimal(1.00)):
+            order.payment_status = PaymentStatus_Paid
+            if order.order_status == OrderItemStatus_Served:
+                order.order_status = "Paid"
         order.save()
         return {
             'status': 200,
