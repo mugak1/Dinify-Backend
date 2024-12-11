394ecac1d63c788c44ac2c718e4abd027af75b9e
Esau Lwanga <31913619+lemndev@users.noreply.github.com>
2024-12-11 18:36:12 +0300

    update endpoint for initiating transactions

--- finance_app/endpoints/transactions.py
+++ finance_app/endpoints/transactions.py
@@ -4,6 +4,7 @@ endpoints to handle order
 from rest_framework.response import Response
 from rest_framework.views import APIView
 from finance_app.controllers.initiate_transaction import initiate_transaction
+from finance_app.controllers.tx_subscription import SubscriptionPaymentTransaction
 
 
 class TransactionsEndpoint(APIView):
@@ -13,12 +14,31 @@ class TransactionsEndpoint(APIView):
 
     def post(self, request):
         data = request.data
-        response = initiate_transaction(
-            transaction_type=data.get('transaction_type'),
-            transaction_platform=data.get('transaction_platform'),
-            actor=request.user,
-            amount=data.get('amount'),
-            otp=data.get('otp'),
-            bank_account=data.get('bank_account'),
-        )
-        return Response(response, status=200)
+        # response = initiate_transaction(
+        #     transaction_type=data.get('transaction_type'),
+        #     transaction_platform=data.get('transaction_platform'),
+        #     actor=request.user,
+        #     amount=data.get('amount'),
+        #     otp=data.get('otp'),
+        #     bank_account=data.get('bank_account'),
+        # )
+        # return Response(response, status=200)
+
+        transaction_type = data.get('transaction_type')
+
+        if transaction_type not in ['subscription', 'disbursement', 'order_refund']:
+            return Response({
+                'status': 400,
+                'message': 'Invalid transaction type'
+            }, status=400)
+
+        if transaction_type == 'subscription':
+            response = SubscriptionPaymentTransaction().initiate(
+                restaurant_id=data.get('restaurant_id'),
+                transaction_platform=data.get('transaction_platform'),
+                payment_mode=data.get('payment_mode'),
+                user=request.user,
+                msisdn=data.get('msisdn'),
+                otp=data.get('otp'),
+            )
+            return Response(response, status=200)
