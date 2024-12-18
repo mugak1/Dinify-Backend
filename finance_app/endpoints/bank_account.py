ea358894a0f0d9a49706663d01f9f0c8a0415912
Esau Lwanga <lemndev@gmail.com>
2024-12-18 19:13:11 +0300

    endpoint to get the list of bank accounts for a restuarant

--- finance_app/endpoints/bank_account.py
+++ finance_app/endpoints/bank_account.py
@@ -17,6 +17,8 @@ REQUIRED_INFORMATION = [
 
 class BankAccountRecordsEndpoint(APIView):
     def post(self, request):
+        # TODO only dinify admins should be able to add bank account records
+ 
         data = request.data
         data = {k: (v.upper() if k not in ['restaurant', 'user'] else v) for k, v in data.items()}
 
@@ -33,3 +35,28 @@ class BankAccountRecordsEndpoint(APIView):
         }
         response = Secretary(secretary_args).create()
         return Response(response, status=201)
+
+    def get(self, request):
+
+        filter = {}
+
+        if 'restaurant' in request.query_params:
+            filter['restaurant'] = request.query_params['restaurant']
+        
+        secretary_args = {
+            'request': request,
+            'serializer': SerializerPutBankAccountRecord,
+            'filter': filter,
+            'paginate': True,
+            'user_id': request.user.id,
+            'username': request.user.username,
+            'success_message': 'The bank accounts have been retrieved successfully.',
+            'error_message': 'Sorry, an error occurred while retrieving the bank accounts. Please try again later.',
+        }
+
+        response = Secretary(secretary_args).read()
+
+        return Response(
+            response,
+            status=response['status']
+        )
