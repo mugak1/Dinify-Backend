9bc202579168df1aea8772f4fb475cfc5d12654a
Esau Lwanga <lemndev@gmail.com>
2025-03-22 13:55:06 +0300

    close #76

--- restaurants_app/endpoints/restaurant_setup.py
+++ restaurants_app/endpoints/restaurant_setup.py
@@ -445,7 +445,7 @@ class RestaurantSetupEndpoint(APIView):
 
         if config_detail == 'menuitems':
             orm_filter['section_group__deleted'] = False
-            orm_filter['section_group__available'] = True
+            # orm_filter['section_group__available'] = True
             if 'available' not in request.GET:
                 orm_filter['available'] = True
 
